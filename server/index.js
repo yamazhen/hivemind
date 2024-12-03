b58701578975fd950bbab3f274979539d5ae60c3
yamazhen <bowxyt@gmail.com>
2024-12-03 14:46:46 +0900

    webhook test

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const port = 80;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+//webhook test
+
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('*', (req,res) => {
